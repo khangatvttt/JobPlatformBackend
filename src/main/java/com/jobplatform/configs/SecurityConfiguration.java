826ff122bed48093102ceaa6eac66b92085224e0
Phi Hung <21110480@student.hcmute.edu.vn>
2024-12-07 22:16:29 +0700

    Update small logic

--- src/main/java/com/jobplatform/configs/SecurityConfiguration.java
+++ src/main/java/com/jobplatform/configs/SecurityConfiguration.java
@@ -53,6 +53,7 @@ public class SecurityConfiguration {
                     .requestMatchers("/statistics/**").hasAnyAuthority(UserAccount.Role.ROLE_ADMIN.name())
                     .requestMatchers(HttpMethod.GET,"/users").hasAnyAuthority(UserAccount.Role.ROLE_ADMIN.name())
                     .requestMatchers("/auth/**","/grantcode/**").permitAll()
+                    .requestMatchers(HttpMethod.GET,"/jobs/**").permitAll()
                     .anyRequest().authenticated())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
