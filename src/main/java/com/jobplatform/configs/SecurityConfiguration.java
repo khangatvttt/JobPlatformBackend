a19aeb3f7bf3f95a0a75c5e6f03ed9573e7c55dd
Phi Hung <21110480@student.hcmute.edu.vn>
2025-03-31 10:40:08 +0700

    Small update

--- src/main/java/com/jobplatform/configs/SecurityConfiguration.java
+++ src/main/java/com/jobplatform/configs/SecurityConfiguration.java
@@ -67,7 +67,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Replace with your frontend URL
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
         configuration.setExposedHeaders(List.of("X-Total-Pages","X-Total-Elements"));
