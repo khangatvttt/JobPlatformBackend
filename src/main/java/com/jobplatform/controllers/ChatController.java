9408b0ea106345ad2398d6408f3920eee9660699
Khang <khangatvttt@gmail.com>
2025-04-13 16:37:39 +0700

    Send notification when chat

--- src/main/java/com/jobplatform/controllers/ChatController.java
+++ src/main/java/com/jobplatform/controllers/ChatController.java
@@ -1,8 +1,10 @@
 package com.jobplatform.controllers;
 
 import com.jobplatform.models.ChatMessage;
+import com.jobplatform.models.UserAccount;
 import com.jobplatform.models.dto.Message;
 import com.jobplatform.repositories.ChatMessageRepository;
+import com.jobplatform.repositories.UserRepository;
 import com.jobplatform.services.ChatMessageService;
 import com.jobplatform.services.FirebaseService;
 import com.jobplatform.services.NotificationService;
@@ -12,6 +14,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
+import java.util.NoSuchElementException;
 
 @Controller
 public class ChatController {
@@ -19,11 +22,15 @@ public class ChatController {
     private final ChatMessageService chatMessageService;
     private final SimpMessagingTemplate messagingTemplate;
     private final FirebaseService firebaseService;
+    private final UserRepository userRepository;
+    private final NotificationService notificationService;
 
-    public ChatController(ChatMessageService chatMessageService, SimpMessagingTemplate messagingTemplate, FirebaseService firebaseService) {
+    public ChatController(ChatMessageService chatMessageService, SimpMessagingTemplate messagingTemplate, FirebaseService firebaseService, UserRepository userRepository, NotificationService notificationService) {
         this.chatMessageService = chatMessageService;
         this.messagingTemplate = messagingTemplate;
         this.firebaseService = firebaseService;
+        this.userRepository = userRepository;
+        this.notificationService = notificationService;
     }
 
     @MessageMapping("/chat")
@@ -31,7 +38,10 @@ public class ChatController {
         String userName = principal.getName();
         message.setSender(userName);
         chatMessageService.createMessage(message);
-        firebaseService.sendNotification(message.getReceiver(), "Có tin nhắn mới từ " + userName);
+        String messageNoti = "Có tin nhắn mới từ " + userName;
+        UserAccount userReceiver = userRepository.findByEmail(message.getReceiver()).orElseThrow(() -> new NoSuchElementException("User not found for notification"));
+        notificationService.addNotification(messageNoti, "" ,userReceiver);
+        firebaseService.sendNotification(userReceiver.getId(), messageNoti);
         messagingTemplate.convertAndSendToUser(
                 message.getReceiver(), "/queue/messages", message);
     }
