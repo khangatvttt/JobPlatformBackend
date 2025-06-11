3af0d0b900b079a90867c8e8b3652d890b29963e
Khang <khangatvttt@gmail.com>
2025-06-11 22:59:58 +0700

    Update for deploy

--- src/main/java/com/jobplatform/configs/SecurityConfiguration.java
+++ src/main/java/com/jobplatform/configs/SecurityConfiguration.java
@@ -69,7 +69,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://jobplatformfrontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
